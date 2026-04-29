b76f6ad180bc7edfea4840a18aa4d1c4b5b30bf7
Jose Flavio <flaviopereiralc@gmail.com>
2026-04-28 23:09:43 -0300

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
+import express from 'express'
 
+const port = 3000
+const app = express()
+
+app.get('/moveis', (req, res) => {
+    res.send('Listagem de filmes')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
